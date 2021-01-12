4100094bb3d03837e34ce0a3e4fb3e27818f94eb
Skalefou <45128700+Skalefou@users.noreply.github.com>
2021-01-12 12:41:36 +0100

    Added a start menu.
    Added a start menu.

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,5 +1,36 @@
 #include <SFML/Graphics.hpp>
 
+enum {START, IN_GAME, GAME_OVER};
+
+unsigned int start(sf::RenderWindow &window) {
+    sf::RectangleShape rect(sf::Vector2f(412.f, 187.f));
+    sf::Font font;
+    sf::Text text[2];
+
+    rect.setFillColor(sf::Color(46, 139, 87));
+    rect.setOutlineThickness(1.f);
+    rect.setOutlineColor(sf::Color(255, 255, 255));
+    rect.setPosition((512 / 2 - 412 / 2), (512/2 - 187/2));
+
+    font.loadFromFile("data/VT323.ttf");
+    for (int i = 0; i < 2; i++)
+        text[i].setFont(font);
+    text[0].setCharacterSize(36);
+    text[1].setCharacterSize(30);
+    text[0].setString("Snake by Skalefou");
+    text[1].setString("Press enter to play\nPress escape to exit");
+    text[0].setPosition((512 / 2 - text[0].getLocalBounds().width / 2), 160);
+    text[1].setPosition(50, 220);
+
+    window.draw(rect);
+    for(int i = 0; i < 2; i++)
+        window.draw(text[i]);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+        return IN_GAME;
+    else
+        return START;
+}
+
 void setIcon(sf::RenderWindow &window) {
     sf::Image icon;
     if (icon.loadFromFile("data/icon.png"))
@@ -8,7 +39,7 @@ void setIcon(sf::RenderWindow &window) {
 
 int main(void) {
     sf::RenderWindow window(sf::VideoMode(512, 512), "Snake", sf::Style::Close);
-    unsigned int sleep = 30;
+    unsigned int sleep = 30, gameStep = START;
 
     setIcon(window);
     window.setVerticalSyncEnabled(true);
@@ -21,8 +52,9 @@ int main(void) {
             if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
                 window.close();
         }
+        if (gameStep == START)
+            gameStep = start(window);
         window.display();
     }
     return 0;
 }
-
